76b4cf5ed96e27b9892d981b374013d3b07e5526
Yasukazu KAWASAKI <yakawa.1128+github@gmail.com>
2020-07-11 15:43:41 +0900

    Rename

--- common/ic/ic.go
+++ common/ic/ic.go
@@ -1,29 +1,33 @@
 package vm
 
-import "fmt"
-
 type OpeCode int
+type JoinMethod int
 
 const (
 	GetTableOpeCode OpeCode = iota + 1
 )
 
+const (
+	InnerJoin JoinMethod = iota + 1
+	LeftJoin
+	RightJoin
+	FullJoin
+)
+
 type Operation interface {
 	OpeCode() OpeCode
-	String() string
 }
 
 type GetTableOpe struct {
 	Table string
 }
 
-func (o *GetTableOpe) String() string {
-	return fmt.Sprintf("Get Table Operation Named By %s", o.Table)
-}
-
 func (o *GetTableOpe) OpeCode() OpeCode {
 	return GetTableOpeCode
 }
 
-type FilterOpe struct {
+type JoinTableOpe struct {
+	LeftTable  string
+	RightTable string
+	Method     JoinMethod
 }
